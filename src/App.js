2c12693c5210960a45f4fa0b95d7a20a63ba4953
RajiDurai <rajalakshmidurai@u.boisestate.edum>
2024-08-01 22:10:40 -0700

    Final commit with comments and cosmetics added to the code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 
 function ParentComponent({ childTitle }) {
   const [value, setValue] = React.useState(0);
-  //const ChildTitle = React.useMemo(() => childTitle, [childTitle]); -- You can't create a constant which is String - immutable with usememo
+/*const ChildTitle = React.useMemo(() => childTitle, [childTitle]); 
+1. Memoizing values is not here on a constant String - immutable
+2. The child is re-rendered everytime eventhough the value is memoized due to each time the reference change on parent state
+3. can memoize onclick function or any expensive compuation using useCallback() if needed */
   console.log("Parent rendering");
   return (
     <div className="Component">
@@ -14,7 +17,7 @@ function ParentComponent({ childTitle }) {
       <ChildComponent title={childTitle} />
       <button
         onClick={() => {
-          setValue((value) => value + 1);
+          setValue((value) => value + 1); // can move this to a callback function
         }}
       >
         Update State on Parent ++
@@ -23,6 +26,7 @@ function ParentComponent({ childTitle }) {
   );
 }
 
+/*Passes the child component to React.Memo to prevent re-rendering the child when parent state changes. This is expensive but the cost on re-rendering everytime with parent is more expendive*/
 const ChildComponent = React.memo(({title}) => {
   console.log("Child rendering");
   return (
@@ -35,9 +39,10 @@ const ChildComponent = React.memo(({title}) => {
 });
 
 export default function App() {
+  console.log("Root rendering");
   return (
     <div>
-      <ParentComponent childTitle={"Child Component"}  />
+      <ParentComponent childTitle={"Child Component"}  /> {/* removed className = "App" as the props is not used anywhere*/}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <App />
 );
+//Removed strict mode so that parent and child are rendered once(though strict mode is useful for debugging in developer environment)
